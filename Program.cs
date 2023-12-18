6cb92607be385bed7cdd88c352ac19906bf142f0
kayo <kayohramos@gmail.com>
2023-12-18 20:29:47 -0300

    Introducao

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿// using ExemploProjeto.Models;
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "fulano";
+// p.Idade = 20;
+// p.Apresentar();
+
+// int a =  Convert.ToInt32(null);
+// int b =  int.Parse(null);
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+// int a = 5;
+// double b = a;
+// int a = 5;
+// long b = a;
+// long a = 5;
+// int b = Convert.ToInt32(a);
+// long a = long.MaxValue;
+// int b = Convert.ToInt32(a);
+// int a = int.MaxValue;
+// long b = a;
+
+// string a = "15-";
+// int b = 0;
+// int.TryParse(a, out b);
+
+// Console.WriteLine(b);
+
+// Console.WriteLine("Digite a letra");
+// string letra = Console.ReadLine();
+
+// switch (letra){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":  
+//         Console.WriteLine("vogal");
+//         break;
+
+//     default:
+//             Console.WriteLine("Não é vogal");
+//             break;
+// }
+
+
+// using ExemploProjeto.Models;
+
+// Calculadora calc = new Calculadora();
+// calc.Somar(10, 30);
+// calc.Subtrair(30,10);
+// calc.Multiplicar(10, 30);
+// calc.Dividir(30,10);
+// calc.Potencia(30,2);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+// calc.RaizQuadrada(9);
+
+
+// EstruturaDeREpeticao er = new EstruturaDeREpeticao();
+// er.LoopFor(5);
+// er.LoopWhile();
+// er.LoopWhile2(5, 0);
+// er.LoopDoWhile();
+// er.LoopDoWhile2(0, 0);
+
+
+// MenuInterativo mi = new MenuInterativo();
+// // mi.ExecutarMenu();
+// mi.ExecutarMenuRefatorado();
+
+// ArrayEListas al = new ArrayEListas();
+
+// al.arrayDeInterios();
+// al.arrayDeInteriosForEach();
+// al.arrayDeInteriosCopy();
+// al.listArray();
